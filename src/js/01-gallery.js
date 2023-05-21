40e434a9609f6ea172ef8ad250a61d019acb82d4
kondyan2022 <kondyan.asusnexus@gmail.com>
2023-05-21 14:00:59 +0300

    move attaching simplelightbox from css to js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const refs = { imageList: document.querySelector('ul.gallery') };
+
+function createGalleryCard({ preview, original, description }) {
+  return `
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+}
+
+function createGalleryList(Items) {
+  refs.imageList.innerHTML = Items.map(item => createGalleryCard(item)).join(
+    ''
+  );
+}
+
+createGalleryList(galleryItems);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
